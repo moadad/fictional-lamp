65745f74ef09df8ddfd26a48b6e92a65807d262a
moadad <moaeplans123@gmail.com>
2026-08-31 17:03:37 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'orders-pwa-v1';
+const CACHE_NAME = 'orders-pwa-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -10,9 +10,18 @@ self.addEventListener('activate', e => {
 });
 self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
+  const isNavigation = e.request.mode === 'navigate';
+  if(isNavigation){
+    e.respondWith(fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html', clone));
+      return res;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
     const clone = res.clone();
     caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
     return res;
-  }).catch(() => caches.match('./index.html'))));
+  })));
 });
